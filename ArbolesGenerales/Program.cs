73290f6ff578db3778cf414cbc23d7d0cdbe0ac0
LadyNunez <laadynz@hotmail.com>
2022-06-08 21:55:40 -0700

    Agregando Nodos

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -1,3 +1,31 @@
 ﻿using System;
 
-namespace ArbolesGenerales
+namespace ArbolesGenerales 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            ArbolGeneral arbol = new ArbolGeneral("A");
+
+            Nodo nodoB = arbol.Insertar("B", arbol.Raiz);
+            Nodo nodoC = arbol.Insertar("C", arbol.Raiz);
+
+            Nodo nodoD = arbol.Insertar("D", nodoB);
+            Nodo nodoE = arbol.Insertar("E", nodoB);
+            Nodo nodoF = arbol.Insertar("F", nodoB);
+
+            Nodo nodoI = arbol.Insertar("I", nodoD);
+
+            Nodo nodoJ = arbol.Insertar("J", nodoF);
+            Nodo nodoK = arbol.Insertar("K", nodoF);
+
+            Nodo nodoG = arbol.Insertar("G", nodoC);
+            Nodo nodoH = arbol.Insertar("H", nodoC);
+
+            Nodo nodoL = arbol.Insertar("L", nodoH);
+
+            string elArbol = arbol.ObtenerArbol(new Nodo("D"));
+        }
+    }
+}
