881e98e5016f82b0157e4ff5fd70b14883eab30c
LadyNunez <laadynz@hotmail.com>
2022-06-08 21:47:14 -0700

    Arboles Generales

---
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class ArbolGeneral
+    {
+        private readonly Nodo raiz;
+        public Nodo Raiz { get { return Raiz; } }
+
+        public ArbolGeneral(string dato) 
+        {
+            raiz = new Nodo(dato);   
+        }
+
+        public Nodo Insertar(string dato, Nodo nodoPadre) 
+        {
+            if (string.IsNullOrWhiteSpace(dato)) 
+            {
+                throw new Exception("No se puede insertar un valor vacio en dato");
+            }
+
+            if (nodoPadre == null) 
+            {
+                throw new Exception("No se puede insertar un nodo, sin especificar el padre");
+            }
+
+            if (nodoPadre.Hijo == null)
+            {
+                nodoPadre.Hijo = new Nodo(dato);
+                return nodoPadre.Hijo;
+            }
+            else 
+            {
+                Nodo hijoActual = nodoPadre.Hijo;
+
+                while (hijoActual.Hermano != null) 
+                {
+                    hijoActual = hijoActual.Hermano;
+                }
+
+                hijoActual.Hermano = new Nodo(dato);
+                return hijoActual.Hermano;
+            }
+        }
+
+        private void Recorrer(Nodo nodo, ref int posicion, ref string datos) 
+        {
+            if (nodo != null) 
+            {
+                string dato = nodo.Dato;
+                int cantidadGuiones = dato.Length + posicion;
+                string datoConGuiones = dato.PadLeft(cantidadGuiones, '-');
+                datos += $"{datoConGuiones}\n";
+
+                if (nodo.Hijo != null) 
+                {
+                    posicion++;
+                    Recorrer(nodo.Hijo, ref posicion, ref datos);
+                    posicion--;
+                }
+
+                if (nodo.Hermano != null && posicion != 0) 
+                {
+                    Recorrer(nodo.Hermano,ref posicion, ref datos); 
+                }   
+            }
+        }
+
+        public string ObtenerArbol(Nodo nodo = null) 
+        {
+            if (nodo == null) 
+            {
+                nodo = raiz;
+            }
+
+            int posicion = 0;
+            string datos = string.Empty;
+
+            Recorrer(nodo, ref posicion, ref datos);
+            return datos;
+        }
+
+        public Nodo Buscar(string dato, Nodo nodoBusqueda = null)
+        {
+            if (nodoBusqueda == null) 
+            {
+                return nodoBusqueda;
+            }
+
+            if (nodoBusqueda.Dato.ToUpper() == dato.ToUpper()) 
+            {
+                return nodoBusqueda;
+            }
+
+            if (nodoBusqueda.Hijo != null) 
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hijo);
+                if (nodoEncontrado == null) 
+                {
+                    return nodoEncontrado;
+                }
+            }
+            if (nodoBusqueda.Hermano != null)
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hermano);
+                if (nodoEncontrado != null) 
+                {
+                    return nodoEncontrado;
+                }
+
+            
+            }
+            return null;
+        }
+
+    }
+}
--- ArbolesGenerales/Nodo.cs
+++ ArbolesGenerales/Nodo.cs
@@ -8,7 +8,20 @@ namespace ArbolesGenerales
 {
     public class Nodo
     {
+        private string dato;
+        private Nodo hijo;
+        private Nodo hermano;
 
+        public string Dato { get => dato; set => dato = value; } 
+        public Nodo Hijo { get => hijo; set => hijo = value; }  
+        public Nodo Hermano { get => hermano; set => hermano = value; }
+
+        public Nodo(string dato, Nodo hijo = null, Nodo hermano = null) 
+        {
+            this.dato = dato;   
+            this.hijo = hijo;
+            this.hermano = hermano;
+        }
 
     }
 }
