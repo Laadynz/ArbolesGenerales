e2f4d3b59387badb0639f7cb2091089df50314b4
LadyNunez <laadynz@hotmail.com>
2022-06-11 11:36:39 -0700

    Arbol Binario

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -7,6 +7,16 @@
             ArbolGeneral arbol = new ArbolGeneral("A");
 
             Nodo nodoB = arbol.InsertarHijo(arbol.Raiz,"B");
+            Nodo nodoC = arbol.InsertarHijo(arbol.Raiz,"C");
+
+            Nodo nodoD = arbol.InsertarHijo(nodoB,"D");
+            Nodo nodoE = arbol.InsertarHijo(nodoB,"E"); 
+            Nodo nodoF = arbol.InsertarHijo(nodoB,"F");
+
+            Nodo nodoI = arbol.InsertarHijo(nodoD,"I");
+
+            Nodo nodoJ = arbol.InsertarHijo(nodoF,"J");
+            Nodo nodoK = arbol.InsertarHijo(nodoF,"K");
 
             string elArbol = arbol.ObtenerArbol();
             Console.WriteLine(elArbol);
